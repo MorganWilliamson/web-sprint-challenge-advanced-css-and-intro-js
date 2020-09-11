f737171dcbf0b2d06500d0ea27e147345dd98577
MorganWilliamson <mwilliamson7695@gmail.com>
2020-09-11 12:49:43 -0500

    Completed Task 7!

--- index.js
+++ index.js
@@ -311,12 +311,17 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(/* Code here */){
-
-  /* Code here */
-
+function lotsOfArt(array){
+  let paintings = [ ];
+    for (let i = 0; i < array.length; i++){
+      if(array[i]["paintings"] > 100){
+        paintings.push(array[i]["name"]);
+      }
+    }
+    return paintings;
 }
 
+console.log(lotsOfArt(artists));
 
 
 // 🎨🎨 STRETCH 🎨🎨//
